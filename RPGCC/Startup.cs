1ac0dee54eec6959ba9205d194d0c620377b3753
CrazyCriple <djsocom@hotmail.com>
2014-04-11 15:42:38 +0200

    First Commit
    Let's gogo

---
+++ RPGCC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RPGCC.Startup))]
+namespace RPGCC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
