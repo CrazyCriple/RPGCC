6308edd8b623d96ad2eaacccd83cc3cf3a41eae0
CrazyCriple <djsocom@hotmail.com>
2014-04-13 13:46:25 +0200

    Damafe

---
+++ RPGCC/Models/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RPGCC.Models
+{
+    public class Damage
+    {
+            public readonly string Name { get; set; }
+            public int Value { get; set; }
+
+
+            public Damage(string name)
+            {
+                Name = name;
+            }
+
+        }
+    }
+}
