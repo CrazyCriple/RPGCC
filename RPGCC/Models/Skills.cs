73db56377ead6d774844342629c1fd48dc457e0b
CrazyCriple <djsocom@hotmail.com>
2014-04-13 14:34:05 +0200

    Skills

---
+++ RPGCC/Models/Skills.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RPGCC.Models
+{
+    public class Skill
+    {
+        public string Name { get; set; }
+        public int Rank { get; set; }
+        public List<string> Specializations { get; set; }
+
+
+    }
+}
