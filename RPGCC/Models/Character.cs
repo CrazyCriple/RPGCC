7f009ef16ab404626ab00fc333cb753bb5fbe047
Tosos85 <jonas.kruettli@me.com>
2014-04-13 15:15:14 +0200

    Character

--- RPGCC/Models/Character.cs
+++ RPGCC/Models/Character.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using RPGCC.Models.Assets;
 
 namespace RPGCC.Models
 {
@@ -14,12 +15,48 @@ namespace RPGCC.Models
         public string Description { get; set; }
         public int Spark { get; set; }
         public int DoomActual { get; set; }
-        public List<String> Traits;
-        public List<String> Consequences;
-        public List<String> Gear;
-        public List<String> Perks;
-        public List<Skill> Skills;
+        public List<String> Traits { get; set; }
+        public List<String> Consequences { get; set; }
+        public List<String> Gear { get; set; }
+        public List<String> Perks { get; set; }
+        public List<Skill> Skills { get; set; }
+        public List<IAsset> Assets { get; set; }
+        public List<IDamage> Damages { get; set; }
 
+        private void FillAssets()
+        {
+            Assets = new List<IAsset>();
+            Assets.Add(new Acuity());
+            Assets.Add(new Force());
+            Assets.Add(new Fortune());
+            Assets.Add(new Grace());
+        }
+
+        private void FillDamage()
+        {
+            Damages = new List<IDamage>();
+            Damages.Add(new Doubt());
+            Damages.Add(new Fear());
+            Damages.Add(new Kismet());
+            Damages.Add(new Pain());
+        }
+
+        private void FillSkills()
+        {
+            Skills = new List<Skill>();
+            Skills.Add(new Skill { Name = "Academia" });
+            Skills.Add(new Skill { Name = "Awareness" });
+            Skills.Add(new Skill { Name = "Athletics" });
+            Skills.Add(new Skill { Name = "Influence" });
+            Skills.Add(new Skill { Name = "Legerdemain" });
+            Skills.Add(new Skill { Name = "Subterfuge" });
+            Skills.Add(new Skill { Name = "Marksmanship" });
+            Skills.Add(new Skill { Name = "Weaponry" });
+        }
+        private void FillCustomSkill(Skill skill)
+        {
+            Skills.Add(skill);
+        }
 
     }
     public enum Gender
