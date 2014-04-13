cdacdaef83f31b04d1fe4f1c9e2445af3ae8f0c5
CrazyCriple <djsocom@hotmail.com>
2014-04-13 15:45:24 +0200

    x

---
+++ RPGCC/Controllers/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RPGCC.Models;
+
+namespace RPGCC.Controllers
+{
+    public class CharacterController : Controller
+    {
+        //
+        // GET: /Character/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Only for testing
+        /// </summary>
+        private void CreateTestChar()
+        {
+            var char1 = new Character("Ragnar Lovbrok");
+
+            char1.Age = 28;
+            char1.Concept = "Impaler";
+            char1.Assets.Find(x => x.Name == "force").Value = 5;
+            char1.Assets.Find(x => x.Name == "acuity").Value = 3;
+            char1.Assets.Find(x => x.Name == "grace").Value = 1;
+
+        }
+	}
+}
